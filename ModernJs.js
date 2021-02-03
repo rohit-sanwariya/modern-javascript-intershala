b2b62aa5dacb3b5e5e64a74d084091fbf2b5474c
rohit-sanwariya <rohitsanwariya1995@gmail.com>
2021-02-03 14:06:32 +0530

    Reference Error : not defined

--- ModernJs.js
+++ ModernJs.js
@@ -6,9 +6,12 @@ function varIsBad(){
     if(true){
         if(true){
             var scopeFunVar = 45;
+            // let scopeFunLet = "this won't print"
         }
     }
     console.log(scopeFunVar);
+    //console.log(scopeFunLet); 
+    // ReferenceError: scopeFunLet is not defined
 }
 
 varIsBad()
